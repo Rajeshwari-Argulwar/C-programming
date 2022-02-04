e1536e0e6856f69de4f2358a68aa88fd981aafea
Rajeshwari-Argulwar <98045986+Rajeshwari-Argulwar@users.noreply.github.com>
2022-02-04 18:57:52 +0530

    Create ascii_value.c

---
+++ ascii_value.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main ()
+{
+    int i;
+    for(i=65;i<=90;i++)
+    {
+        printf("%c - %d\n",i,i); // we can print ASCII value by useing "%d" and can print charecter by"%c"
+    }
+}
